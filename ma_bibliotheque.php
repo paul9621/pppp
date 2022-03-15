9ed91e584d86eefa6922fb08ec5abf3cb9b0fa3e
paul9621 <98022867+paul9621@users.noreply.github.com>
2022-03-15 16:11:16 +0100

    Update ma_bibliotheque.php

--- ma_bibliotheque.php
+++ ma_bibliotheque.php
@@ -5,7 +5,7 @@ function trouver_min($tableau) {
     $min = $tableau[0];
 
     foreach ($tableau as $element) {
-        if ($min < $element) {
+        if ($min >= $element) {
             $min = $element;
         }
     }
@@ -15,7 +15,15 @@ function trouver_min($tableau) {
 
 // Fonction pour trouver la valeur maximale d'un tableau
 function trouver_max($tableau) {
-    // TODO: Écrire le corps de la fonction
+      $max = $tableau[0];
+
+    foreach ($tableau as $element) {
+        if ($max <= $element) {
+            $max = $element;
+        }
+    }
+
+    return $max;
 }
 
 // Fonction pour trouver la valeur la plus proche de zéro
